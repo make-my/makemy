3ef31513d4c94636b54acece73db86203732676c
MathiasWP <mathiaswpicker@gmail.com>
2020-04-16 01:01:13 +0200

    goodnite

--- cli/makeproject.js
+++ cli/makeproject.js
@@ -1,11 +1,5 @@
 #! /usr/bin/env node
 
-let makemy = null;
-
-try {
-  makemy = require(`${process.cwd()}../node_modules/makemy`);
-} catch (e) {
-  makemy = require('makemy');
-}
+const makemy = require('makemy');
 
 makemy.init(process.cwd());
